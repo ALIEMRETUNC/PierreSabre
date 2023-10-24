e8cf364e0c18f40fd09ec2f780bb05fdea398a84
ALIEMRETUNC <ali-emre.tunc@univ-tlse3.fr>
2023-10-24 20:01:39 +0200

    creation de classe Yakuza

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,14 +1,14 @@
 package histoire;
 
 import personnages.Commercant;
+import personnages.Yakuza;
 
 public class HistoireTP4 {
 	public static void main(String[] args) {
-		Commercant marco = new Commercant("Marco", 20);
+		Commercant marco = new Commercant("Marco", 15);
+        Yakuza yakuza = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
 
-        marco.direBonjour();
-        int argentPerdu = marco.seFaireExtorquer();
-        marco.recevoir(15);
-        marco.boire();
+        yakuza.direBonjour();
+        yakuza.extorquer(marco);
 	}
 }
